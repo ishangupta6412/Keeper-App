2525d1c46cc6c9207308b963f8e74164c0f8fa53
Ishan Gupta <75711595+ishangupta6412@users.noreply.github.com>
2021-08-12 21:35:21 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App (){
+    return(
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note () {
+    return(
+    <>
+        <div className="note">
+            <h1>Hey</h1>
+            <p>I am a React Developer</p>
+        </div>
+    </>
+    );
+}
+
+export default Note;
